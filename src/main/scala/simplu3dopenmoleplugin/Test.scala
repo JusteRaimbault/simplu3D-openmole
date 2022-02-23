ef24140272f3f3d5f829d57efdb460111268627f
Juste Raimbault <juste.raimbault@ign.fr>
2022-02-23 09:36:23 +0100

    json

--- src/main/scala/simplu3dopenmoleplugin/Test.scala
+++ src/main/scala/simplu3dopenmoleplugin/Test.scala
@@ -13,7 +13,7 @@ object Test extends App {
 
   def runSimplu3dTask(): Unit = {
     val res = Simplu3DTask.apply(new File("src/main/resources/data"), outputFolder = new File("res"),
-      paramFile = new File("src/main/resources/scenario/building_parameters_project_expthese_temp.xml"),
+      paramFile = new File("src/main/resources/scenario/parameters_iauidf.json"),
       distReculVoirie = 1.0, distReculFond = 1.0, distReculLat = 1.0, maximalCES = 0.5,
       hIniRoad = 5.0, slopeRoad = 1.5, hauteurMax = 20.0, seed = 42L
     )
